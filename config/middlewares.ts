26758dd5474e1f7ff3d0f04d856644a936ad7e19
xiangyun <xiangyun@MacBook-Air-13.local>
2025-09-19 14:38:52 +0800

    Initial commit

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -2,7 +2,21 @@ export default [
   'strapi::logger',
   'strapi::errors',
   'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::cors',
+    config: {
+      enabled: true,
+      origin: [
+        'http://localhost:5173', // Vite 本地
+        'http://localhost:5174', // 备用端口
+        'https://student-3cx.pages.dev', // 你的 Cloudflare Pages 默认域名
+      ],
+      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
+      headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
+      keepHeaderOnError: true,
+      credentials: true, // 如果前端需要带 cookie / auth headers，记得加
+    },
+  },
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
--- config/server.ts
+++ config/server.ts
@@ -1,6 +1,7 @@
 export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  url: env('PUBLIC_URL', 'https://intuitive-basket-560a2813ce.strapiapp.com'),
   app: {
     keys: env.array('APP_KEYS'),
   },
